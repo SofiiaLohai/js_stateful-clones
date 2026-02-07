6e3ca45e89ef6fad5f1d184ba41557f2e2f744a2
Sofiia Lohai <logay1810@gmail.com>
2026-02-07 18:33:59 +0100

    Task Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const results = [];
+
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    let newState;
+
+    if (action.type === 'clear') {
+      newState = {};
+    } else if (action.type === 'addProperties') {
+      newState = { ...currentState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      newState = { ...currentState };
+
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+    }
+
+    results.push(newState);
+    currentState = newState;
+  }
+
+  return results;
 }
 
 module.exports = transformStateWithClones;
